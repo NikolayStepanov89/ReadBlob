e247030ed2824522c31a436de6718936fdd86d02
Stepanov N <po4t@yandex.ru>
2018-03-04 23:08:59 +0300

    Add project files.

---
+++ ReadBlob/DBUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace Tutorial.SqlConn
+{
+    class DBUtils
+    {
+        public static OracleConnection GetDBConnection()
+        {
+            string host = ConfigurationSettings.AppSettings["host"];   //"192.168.10.221";
+            int port = Convert.ToInt32(ConfigurationSettings.AppSettings["port"]);
+            string sid = ConfigurationSettings.AppSettings["sid"];
+            string user = ConfigurationSettings.AppSettings["user"];
+            string password = ConfigurationSettings.AppSettings["password"];
+
+            return DBOracleUtils.GetDBConnection(host, port, sid, user, password);
+        }
+    }
+
+}
---
+++ ReadBlob/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Oracle.ManagedDataAccess.Client;
+using Tutorial.SqlConn;
+
+namespace ReadBlob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OracleConnection conn = DBUtils.GetDBConnection();
+            int j = 1;
+            string sql = ConfigurationSettings.AppSettings["select"];
+
+            Console.WriteLine("GetConnection:" + conn);
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+
+            //using (FileStream fstream = new FileStream(@"C:\123\5555\"+j+".xml", FileMode.OpenOrCreate))
+
+            try
+            {
+                conn.Open();
+                OracleDataReader dr = cmd.ExecuteReader();
+                Console.WriteLine(conn.ConnectionString, "Successful Connection");
+
+
+                while (dr.Read())
+                {
+                    // File.Create(@"C:\123\5555\" + j + ".xml");
+                    StreamWriter sw = new StreamWriter(File.Create(ConfigurationSettings.AppSettings["path"] + j + ".xml"));
+                    //   byte[] array = Encoding.Default.GetBytes(dr.GetString(0));
+                    // fstream.Write(array, 0, array.Length);
+                    sw.WriteLine(dr.GetString(0));
+                    sw.Close();
+                    j = j + 1;
+                   // Console.WriteLine(dr.GetOracleBlob(0).ToString());
+                }
+
+                conn.Close();
+
+            }
+            catch (Exception)
+            {
+                //  Console.WriteLine("## ERROR: " + ex.Message);
+                Console.Read();
+                return;
+
+
+            }
+            Console.WriteLine("Connection successful!");
+
+            Console.Read();
+
+        }
+    }
+}
